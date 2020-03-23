0b86754bf12753e50ddfe5222debb6b642bf68e8
Vanda Adams <vandamadams@gmail.com>
2020-03-23 10:18:14 +0000

    added css styling to marquee element

---
+++ javascript/fifty.js
@@ -0,0 +1,6 @@
+const title = 'FIFTY Music Festival — November 10–12, Desert Valley'
+
+// creating an array with 50 empty elements and filling them with title
+const marqueeText = new Array(50).fill(title);
+
+//console.log(marqueeText);
