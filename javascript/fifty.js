91e7fc113ae85cd590b77dcd101eadc3655400cb
Vanda Adams <vandamadams@gmail.com>
2020-03-23 10:57:27 +0000

    updated js function for marquee element

--- javascript/fifty.js
+++ javascript/fifty.js
@@ -1,9 +1,12 @@
-const title = 'FIFTY Music Festival — November 10–12, Desert Valley'
-// creating an array with 50 empty elements and filling them with title
-const marqueeText = new Array(50).fill(title);
-// grabs marquee span from html
-const marquee = document.querySelector('.marquee span')
-// sets repeating title as the content
-marquee.innerHTML = marqueeText.join(' — ')
+function makeMarquee () {
+  const title = 'FIFTY Music Festival — November 10–12, Desert Valley'
+  // creating an array with 50 empty elements and filling them with title
+  const marqueeText = new Array(50).fill(title);
+  // grabs marquee span from html
+  const marquee = document.querySelector('.marquee span')
+  // sets repeating title as the content
+  marquee.innerHTML = marqueeText.join(' — ')
+  //console.log(marquee)
+}
 
-//console.log(marquee)
+makeMarquee()
