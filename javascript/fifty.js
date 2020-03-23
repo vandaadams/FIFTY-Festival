02ae5d14406571622d5c71466e3918431f817e15
Vanda Adams <vandamadams@gmail.com>
2020-03-23 10:53:47 +0000

    added js to populate marquee span

--- javascript/fifty.js
+++ javascript/fifty.js
@@ -1,6 +1,9 @@
 const title = 'FIFTY Music Festival — November 10–12, Desert Valley'
-
 // creating an array with 50 empty elements and filling them with title
 const marqueeText = new Array(50).fill(title);
+// grabs marquee span from html
+const marquee = document.querySelector('.marquee span')
+// sets repeating title as the content
+marquee.innerHTML = marqueeText.join(' — ')
 
-//console.log(marqueeText);
+//console.log(marquee)
