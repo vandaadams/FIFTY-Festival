c07af550c94ac0c45eed6ba801339f75192b33ac
Vanda Adams <vandamadams@gmail.com>
2020-03-25 14:16:14 +0000

    added in-view feature

--- javascript/fifty.js
+++ javascript/fifty.js
@@ -1,7 +1,7 @@
-function makeMarquee () {
+function makeMarquee() {
   const title = 'FIFTY Music Festival — November 10–12, Desert Valley'
   // creating an array with 50 empty elements and filling them with title
-  const marqueeText = new Array(50).fill(title);
+  const marqueeText = new Array(50).fill(title)
   // grabs marquee span from html
   const marquee = document.querySelector('.marquee span')
   // sets repeating title as the content
@@ -13,7 +13,7 @@ makeMarquee()
 
 // creates random number
 const random = (min, max) => {
-    return Math.floor(Math.random() * (max-min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 // selects all circle elements
@@ -35,15 +35,30 @@ const squiggles = document.querySelectorAll('.squiggle')
 squiggles.forEach((squiggle, index) => {
   // creates random number between 0 and 45
   const randomNumber = random(0, 45)
-  squiggle.animate([
-    { transform: 'rotate(0deg)' },
- 		// adds random number to rotate
-     { transform: `rotate(${randomNumber}deg)` },
-    { transform: 'rotate(0deg)' }
-  ], {
-    // animation delay
-    delay: 300 * index,
-    duration: 5000,
-    iterations: Infinity
-  });
+  squiggle.animate(
+    [
+      {transform: 'rotate(0deg)'},
+      // adds random number to rotate
+      {transform: `rotate(${randomNumber}deg)`},
+      {transform: 'rotate(0deg)'}
+    ],
+    {
+      // animation delay
+      delay: 300 * index,
+      duration: 5000,
+      iterations: Infinity
+    }
+  )
 })
+
+inView('.section')
+  .on('enter', section => {
+    // vanilla JS version of jQuery's .addClass() method
+    section.classList.add('in-viewport')
+  })
+  .on('exit', section => {
+    section.classList.remove('in-viewport')
+  })
+
+  // only adds the class once the user has scrolled 0.2 of the section of the viewport
+  inView.threshold(0.2);
